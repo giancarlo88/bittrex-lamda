c69f19d7f873fac900ad3b1c86c02e5017a6775a
Giancarlo Galliani <galliani.08@gmail.com>
2017-11-20 22:42:26 +0000

    Build api calls and logging.

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: false,
+    node: true,
+    es6: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    sourceType: 'require',
+    allowImportExportEverywhere: false,
+    codeFrame: false
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'no-console': ['warn', 'never']
+  }
+}
--- server.js
+++ server.js
@@ -1,22 +1,54 @@
-import request from 'superagent'
-
+require('dotenv').config()
+const request = require('superagent')
 const url = 'https://bittrex.com/api/v1.1'
+const crypto = require('crypto')
+const chalk = require('chalk')
 
-const getBalances = async () => { 
-  try {
-    const res = request.get(`${url}/account/getbalances?apikey=${process.env.API-KEY}`)
-    return res
-  }
-  catch (err) { 
-    throw new Error(`There was an error:\n ${err}`)
-  }
+const log = console.log
+
+const makeSign = uri => {
+  return crypto
+    .createHmac('sha512', process.env.API_SECRET)
+    .update(uri)
+    .digest('hex')
 }
 
-const update = async () => { 
-  const res = await Promise.all(getBalances)
-  console.log(res)
+const makeNonce = () => Date.now()
+const getBalances = async () => {
+  const uri = `${url}/account/getbalances?apikey=${process.env.API_KEY}&nonce=${makeNonce()}`
+  const sign = makeSign(uri)
+  const res = await request.get(uri).set('apisign', sign)
+  if (res.body && res.body.success) return res.body
+  else throw res.body.message
 }
 
-const start = () => setInterval(update, 30000)
+const getTicker = async market => {
+  const uri = `${url}/public/getticker?market=${market}`
+  const res = await request.get(uri)
+  if (res.body && res.body.success) return res.body
+  else throw res.body.message
+}
 
-start()
+const update = () => {
+  log(chalk.cyanBright('🤔  Updating! 🤔'))
+  console.clear()
+  Promise.all([getBalances(), getTicker('BTC-LTC'), getTicker('BTC-NAV')])
+    .then(res => {
+      log(chalk.cyanBright('💶 💷  My currencies 💷 💶 \n'))
+      res[0].result.forEach(currency =>
+        log(chalk.green(JSON.stringify(currency) + '\n'))
+      )
+      log(chalk.cyanBright('💵 💴  Current prices 💴 💵 \n'))
+      res.forEach((response, index) => {
+        if (index > 0) {
+          log(chalk.yellow(JSON.stringify(response.result) + '\n'))
+        }
+      })
+    })
+    .catch(err => {
+      throw new Error(chalk.red(`🔥 There was an error 🔥:\n ${err}`))
+    })
+}
+
+update()
+const start = setInterval(update, 10000)
