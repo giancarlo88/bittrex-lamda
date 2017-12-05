c3d434f08b6cafbbc34a842d90af6e1fb2d301cd
Giancarlo Galliani <galliani.08@gmail.com>
2017-12-05 23:46:14 +0000

    Add function to save to dynamoDB.

---
+++ aws.js
@@ -0,0 +1,36 @@
+const AWS = require('aws-sdk')
+
+const updateDB = value => {
+  AWS.config.update({
+    region: 'eu-west-2',
+    endpoint: 'dynamodb.eu-west-2.amazonaws.com'
+  })
+
+  const client = new AWS.DynamoDB.DocumentClient()
+
+  const table = 'btx-tracker'
+
+  const date = new Date(Date.now())
+
+  var params = {
+    TableName: table,
+    Item: {
+      Date: date,
+      price: value
+    }
+  }
+
+  console.log('Adding a new item...')
+  client.put(params, function(err, data) {
+    if (err) {
+      console.error(
+        'Unable to add item. Error JSON:',
+        JSON.stringify(err, null, 2)
+      )
+    } else {
+      console.log('Added item:', JSON.stringify(data, null, 2))
+    }
+  })
+}
+
+module.exports = updateDB
--- index.js
+++ index.js
@@ -2,83 +2,84 @@ require('dotenv').config()
 const crypto = require('crypto')
 const request = require('superagent')
 const calculate = require('./calculate')
-const updateSheet = require('./google')
+const updateDB = require('./aws')
 const url = 'https://bittrex.com/api/v1.1'
+exports.handler = function index(event, context, callback) {
+  const makeSign = uri => {
+    return crypto
+      .createHmac('sha512', process.env.API_SECRET)
+      .update(uri)
+      .digest('hex')
+  }
 
-const makeSign = uri => {
-  return crypto
-    .createHmac('sha512', process.env.API_SECRET)
-    .update(uri)
-    .digest('hex')
-}
+  const makeNonce = () => Date.now()
+  const getCurrencies = () =>
+    new Promise((resolve, reject) => {
+      const uri = `${url}/account/getbalances?apikey=${process.env.API_KEY}&nonce=${makeNonce()}`
+      const apisign = makeSign(uri)
 
-const makeNonce = () => Date.now()
-const getCurrencies = () =>
-  new Promise((resolve, reject) => {
-    const uri = `${url}/account/getbalances?apikey=${process.env.API_KEY}&nonce=${makeNonce()}`
-    const apisign = makeSign(uri)
+      request
+        .get(uri)
+        .set('apisign', apisign)
+        .then(res => {
+          if (res.body && res.body.success) {
+            return resolve(res.body)
+          }
+          return reject(res.body.message)
+        })
+        .catch(err => {
+          return reject(err)
+        })
+    })
 
-    request
-      .get(uri)
-      .set('apisign', apisign)
-      .then(res => {
-        if (res.body && res.body.success) {
-          return resolve(res.body)
-        }
-        return reject(res.body.message)
-      })
-      .catch(err => {
-        return reject(err)
-      })
+  const getTicker = market =>
+    new Promise((resolve, reject) => {
+      const uri = `${url}/public/getticker?market=${market}`
+      request
+        .get(uri)
+        .then(res => {
+          if (res.body && res.body.success) return resolve(res.body.result)
+          return reject(res.body.message)
+        })
+        .catch(err => {
+          return reject(err)
+        })
+    })
+
+  const makeTickerRequests = currencies => {
+    console.log(currencies)
+    return currencies.result.map(
+      ({ Currency: currency, Available: available }) =>
+        new Promise(resolve => {
+          if (currency === 'BTC') return resolve({ currency, available })
+          getTicker(`BTC-${currency}`).then(res =>
+            resolve({
+              currency,
+              available: Number(available),
+              price: Number(res.Last)
+            })
+          )
+        })
+    )
+  }
+
+  const mapTickers = currencies => {
+    const tickers = Promise.all(makeTickerRequests(currencies))
+    return Promise.resolve(tickers)
+  }
+
+  const update = () =>
+    new Promise((resolve, reject) => {
+      getCurrencies()
+        .then(currencies => mapTickers(currencies))
+        .then(results => resolve(results))
+        .catch(err => {
+          return reject(err)
+        })
+    })
+
+  update().then(res => {
+    const value = calculate(res)
+    updateDB(value)
   })
-
-const getTicker = market =>
-  new Promise((resolve, reject) => {
-    const uri = `${url}/public/getticker?market=${market}`
-    request
-      .get(uri)
-      .then(res => {
-        if (res.body && res.body.success) return resolve(res.body.result)
-        return reject(res.body.message)
-      })
-      .catch(err => {
-        return reject(err)
-      })
-  })
-
-const makeTickerRequests = currencies => {
-  console.log(currencies)
-  return currencies.result.map(
-    ({ Currency: currency, Available: available }) =>
-      new Promise(resolve => {
-        if (currency === 'BTC') return resolve({ currency, available })
-        getTicker(`BTC-${currency}`).then(res =>
-          resolve({
-            currency,
-            available: Number(available),
-            price: Number(res.Last)
-          })
-        )
-      })
-  )
 }
-
-const mapTickers = currencies => {
-  const tickers = Promise.all(makeTickerRequests(currencies))
-  return Promise.resolve(tickers)
-}
-
-const update = () =>
-  new Promise((resolve, reject) => {
-    getCurrencies()
-      .then(currencies => mapTickers(currencies))
-      .then(results => resolve(results))
-      .catch(err => {
-        return reject(err)
-      })
-  })
-
-update().then(res => {
-  const value = calculate(res)
-  updateSheet(value)
-})
