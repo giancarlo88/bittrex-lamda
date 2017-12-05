acb140c1705b484cffd779f46aa1384e9b937450
Giancarlo Galliani <galliani.08@gmail.com>
2017-12-05 07:52:15 +0000

    Remove api key.

--- google.js
+++ google.js
@@ -3,7 +3,7 @@ const request = require('superagent')
 const updateSheet = value => {
   request
     .post(
-      'https://sheets.googleapis.com/v4/spreadsheets/1eUB4t0WObwGK9wsw22rtIVLC5_NsuQaHG4rkVtCkVk8/values/Sheet1!A1:C1:append?valueInputOption=USER_ENTERED&key=1eUB4t0WObwGK9wsw22rtIVLC5_NsuQaHG4rkVtCkVk8'
+      'https://sheets.googleapis.com/v4/spreadsheets/1eUB4t0WObwGK9wsw22rtIVLC5_NsuQaHG4rkVtCkVk8/values/Sheet1!A1:C1:append?valueInputOption=USER_ENTERED&key=${process.env.GOOGLE_KEY}'
     )
     .send({
       range: 'Sheet1!A1:B1',
